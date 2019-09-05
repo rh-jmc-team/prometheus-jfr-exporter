011646ad92ac2dec1b59ca41834e8d382905c945
kxu <kxu@redhat.com>
2019-09-05 16:57:22 -0400

    Implement argument/option parsing

--- src/main/java/com/redhat/rhjmc/prometheus_jfr_exporter/Main.java
+++ src/main/java/com/redhat/rhjmc/prometheus_jfr_exporter/Main.java
@@ -3,17 +3,35 @@ package com.redhat.rhjmc.prometheus_jfr_exporter;
 import org.eclipse.core.runtime.RegistryFactory;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.nio.file.Files;
 import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] tokens) throws Exception {
 		System.setProperty("org.openjdk.jmc.common.security.manager", SecurityManager.class.getName());
-
 		RegistryFactory.setDefaultRegistryProvider(new RegistryProvider());
 
-		RecordingService rs = new RecordingService("localhost", 0, "/tmp/default.jfc");
+		Config config = null;
+		try {
+			config = parseCommand(tokens);
+		} catch (IllegalArgumentException e) {
+			e.printStackTrace(System.err);
+			System.err.println();
+
+			printHelp(System.err);
+
+			System.exit(1);
+		}
+
+		RecordingService rs = new RecordingService(config.host, config.port, config.eventOptions);
 		rs.start();
 
 		for (int i : new int[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}) {
@@ -24,5 +42,89 @@ public class Main {
 		}
 
 		rs.stop();
+	}
+
+	private static Config parseCommand(String[] tokens) {
+		Map<String, String> options = new HashMap<>();
+		List<String> arguments = new ArrayList<>();
+		{
+			String key = null;
+			for (String token : tokens) {
+				if (token.indexOf("-") == 0) {
+					key = token.substring(1);
+					options.put(key, "");
+					continue;
+				}
+				if (key != null) {
+					options.put(key, token);
+					key = null;
+					continue;
+				}
+				arguments.add(token);
+			}
+		}
+
+		Config config = new Config();
+
+		if (arguments.size() < 1) { // TODO: confirm max arg count
+			throw new IllegalArgumentException("Too few arguments");
+		}
+
+		if (arguments.size() > 2) { // TODO: confirm max arg count
+			throw new IllegalArgumentException("Too many arguments");
+		}
+
+		config.host = arguments.get(0);
+		config.port = arguments.get(1) == null ? JfrConnection.DEFAULT_PORT : Integer.parseInt(arguments.get(1));
+
+		for (Map.Entry<String, String> option : options.entrySet()) {
+			switch (option.getKey()) {
+			case "max-size":
+				try {
+					config.maxSize = Long.parseLong(option.getValue());
+				} catch (NumberFormatException e) {
+					throw new IllegalArgumentException(e);
+				}
+				break;
+			case "max-age":
+				try {
+					config.maxAge = Long.parseLong(option.getValue());
+				} catch (NumberFormatException e) {
+					throw new IllegalArgumentException(e);
+				}
+				break;
+			case "event-options":
+				try {
+					config.eventOptions = new FileInputStream(option.getValue());
+				} catch (FileNotFoundException e) {
+					throw new IllegalArgumentException(e);
+				}
+			default:
+				throw new IllegalArgumentException("Unrecognized option: " + option.getKey());
+			}
+		}
+
+		if (config.eventOptions == null) {
+			config.eventOptions = Main.class.getResourceAsStream("default.jfc");
+		}
+
+		return config;
+	}
+
+	private static void printHelp(PrintStream ps) {
+		ps.println("Usage of Prometheus JFR exporter:");
+		ps.println("	program <host> [port] [option...]");
+		ps.println();
+		ps.println("Options:");
+		ps.println("	--max-size <long>		how much data is kept in the disk repository");
+		ps.println("	--max-age <long>		how far back data is kept in the disk repository");
+	}
+
+	static class Config {
+		String host;
+		Integer port;
+		Long maxAge;
+		Long maxSize;
+		InputStream eventOptions;
 	}
 }
--- src/main/java/com/redhat/rhjmc/prometheus_jfr_exporter/RecordingService.java
+++ src/main/java/com/redhat/rhjmc/prometheus_jfr_exporter/RecordingService.java
@@ -3,6 +3,7 @@ package com.redhat.rhjmc.prometheus_jfr_exporter;
 import org.openjdk.jmc.common.unit.IConstrainedMap;
 import org.openjdk.jmc.common.unit.IQuantity;
 import org.openjdk.jmc.common.unit.QuantityConversionException;
+import org.openjdk.jmc.common.unit.UnitLookup;
 import org.openjdk.jmc.flightrecorder.configuration.events.EventOptionID;
 import org.openjdk.jmc.flightrecorder.configuration.recording.RecordingOptionsBuilder;
 import org.openjdk.jmc.flightrecorder.controlpanel.ui.configuration.model.xml.XMLModel;
@@ -12,12 +13,9 @@ import org.openjdk.jmc.rjmx.services.jfr.FlightRecorderException;
 import org.openjdk.jmc.rjmx.services.jfr.IFlightRecorderService;
 import org.openjdk.jmc.rjmx.services.jfr.IRecordingDescriptor;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.ParseException;
-
-import static org.openjdk.jmc.common.unit.UnitLookup.EPOCH_MS;
 
 public class RecordingService {
 
@@ -27,23 +25,22 @@ public class RecordingService {
 	private IFlightRecorderService mService;
 	private IRecordingDescriptor mRecordingDescriptor;
 
-	private IQuantity mLastScrape = EPOCH_MS.quantity(System.currentTimeMillis());
+	private IQuantity mLastScrape = UnitLookup.EPOCH_MS.quantity(System.currentTimeMillis());
 
-	public RecordingService(String host, String eventOptionFile)
+	public RecordingService(String host, InputStream eventOptionInput)
 			throws IOException, InterruptedException, ServiceNotAvailableException, QuantityConversionException,
 			ParseException {
-		this(host, JfrConnection.DEFAULT_PORT, eventOptionFile);
+		this(host, JfrConnection.DEFAULT_PORT, eventOptionInput);
 	}
 
-	public RecordingService(String host, int port, String eventOptionFile)
+	public RecordingService(String host, int port, InputStream eventOptionFile)
 			throws IOException, InterruptedException, ServiceNotAvailableException, QuantityConversionException,
 			ParseException {
 		mService = new JfrConnection(host, port).getService();
 
 		// TODO: load configurations
 		mRecordingOptions = new RecordingOptionsBuilder(mService).build();
-		InputStream in = new FileInputStream(eventOptionFile);
-		XMLModel model = EventConfiguration.createModel(in);
+		XMLModel model = EventConfiguration.createModel(eventOptionFile);
 		mEventOptions = new EventConfiguration(model)
 				.getEventOptions(mService.getDefaultEventOptions().emptyWithSameConstraints());
 	}
@@ -66,7 +63,7 @@ public class RecordingService {
 	}
 
 	public InputStream openRecording() throws FlightRecorderException {
-		IQuantity now = EPOCH_MS.quantity(System.currentTimeMillis());
+		IQuantity now = UnitLookup.EPOCH_MS.quantity(System.currentTimeMillis());
 		InputStream is = mService.openStream(mRecordingDescriptor, mLastScrape, now, false);
 		mLastScrape = now;
 
